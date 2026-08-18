7192d99f6d2dcdde81b45fe309ad29e13b9ae447
lakshyaanand121 <lakshyaanand121@gmail.com>
2026-08-18 23:16:22 +0530

    Add files via upload
    python-1st day .

---
+++ 1.py
@@ -0,0 +1,13 @@
+print("hello world",end=' ')
+print('hello',1,1.11,'hey',sep='/')
+list=[1,2,3,4,5]
+print(list)
+tuple=(1,2,3,4,5)
+print(tuple)
+sets={1,2,3,4,5}
+print(sets)
+dictionary={'animal':'cat'}
+print(dictionary)
+print(type(list))
+name= input("enter name: ")
+print(name) 
